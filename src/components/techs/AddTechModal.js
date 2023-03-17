ed6dae8393d0030cd4e6b4488d01d7546fbd9a6d
mariellecarnecer99 <mayangcarnecer99@gmail.com>
2023-03-17 11:04:55 +0800

    delete technicians

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addTech } from '../../actions/techActions';
-import M from 'materialize-css/dist/js/materialize.min.js'
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 const AddTechModal = ({addTech}) => {
     const [firstName, setFirstName] = useState('');
