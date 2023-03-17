cf077c435133c34459854b2314db0e6a71780117
mariellecarnecer99 <mayangcarnecer99@gmail.com>
2023-03-17 10:48:55 +0800

    tech reducer action and component

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import TechSelectOptions from '../techs/TechSelectOptions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import { updateLog } from '../../actions/logActions';
 
@@ -52,9 +53,7 @@ const EditLogModal = ({current, updateLog}) => {
                 <div className="input-field">
                     <select name="tech" value={tech} className="browser-default" onChange={e => setTech(e.target.value)}>
                         <option value="" disabled>Select Technician</option>
-                        <option value="John Doe">John Doe</option>
-                        <option value="Sam Smith">Sam Smith</option>
-                        <option value="Sara Wilson">Sara Wilson</option>
+                        <TechSelectOptions />
                     </select>
                 </div>
             </div>
